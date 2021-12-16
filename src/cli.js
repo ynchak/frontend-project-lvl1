f2a4962d9b79471e335d8e7d85a2882a69e45016
Yanchak Yevhen <yanchakevgeniy@gmail.com>
2021-12-16 19:42:56 +0200

    update greeting function

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,6 @@ const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
 export default greeting;
