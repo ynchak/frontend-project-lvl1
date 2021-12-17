35f37891221222069c91dcf320f24f7fccb16078
Yanchak Yevhen <yanchakevgeniy@gmail.com>
2021-12-17 13:16:30 +0200

    moved generate number in new file

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,12 +1,12 @@
 import { cons } from '@hexlet/pairs';
 import game from '../index.js';
+import generateRandomNumber from '../util.js';
 
-const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
 const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
 const even = () => {
   const pairs = [];
   for (let i = 0; i < 3; i += 1) {
-    const question = generateRandomNumber();
+    const question = generateRandomNumber(100);
     const answer = isEven(question);
     const pair = cons(question, answer);
     pairs.push(pair);
---
+++ src/util.js
@@ -0,0 +1,2 @@
+const generateRandomNumber = (max) => Math.floor(Math.random() * max) + 1;
+export default generateRandomNumber;
